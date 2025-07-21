7afc19d1b7bc4f9579d57306d1b73cc613c06e42
amir <amirtaki623@gmail.com>
2025-07-21 23:03:57 +0330

    querySelect at project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 const quoteApiUrl = "https:/api.quotable.io/random?minLength=80&maxLenght=100"
+const quoteSection = document.getElementById("qoute")
+const userInput = document.getElementById("quote-input")
 
 
+console.log( quoteSection, userInput)
